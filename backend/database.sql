c403a01fee44a5773cc3bb6f32cbbc8e096fe5b2
Priya9702 <110077324+Priya9702@users.noreply.github.com>
2026-03-10 22:52:48 +0530

    Update database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE job_tracker;
 
+USE job_tracker;
+
+CREATE TABLE applications (
+id INT AUTO_INCREMENT PRIMARY KEY,
+company VARCHAR(100),
+position VARCHAR(100),
+status VARCHAR(50),
+notes TEXT
+);
