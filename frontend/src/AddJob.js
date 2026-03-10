93895243d38a6ae5e1e64eec83e9734e66f7307c
Priya9702 <110077324+Priya9702@users.noreply.github.com>
2026-03-10 22:57:22 +0530

    Update AddJob.js

--- frontend/src/AddJob.js
+++ frontend/src/AddJob.js
@@ -1,1 +1,95 @@
+import React,{useState} from "react";
+import axios from "axios";
 
+function AddJob(){
+
+const [company,setCompany]=useState("");
+const [position,setPosition]=useState("");
+const [status,setStatus]=useState("");
+const [notes,setNotes]=useState("");
+
+const addJob = async ()=>{
+
+await axios.post("http://127.0.0.1:5000/applications",{
+company,
+position,
+status,
+notes
+});
+
+alert("Application Added");
+
+}
+
+return(
+
+<div>
+
+<input placeholder="Company"
+onChange={(e)=>setCompany(e.target.value)}/>
+
+<input placeholder="Position"
+onChange={(e)=>setPosition(e.target.value)}/>
+
+<input placeholder="Status"
+onChange={(e)=>setStatus(e.target.value)}/>
+
+<input placeholder="Notes"
+onChange={(e)=>setNotes(e.target.value)}/>
+
+<button onClick={addJob}>Add</button>
+
+</div>
+
+)
+
+}
+
+export default AddJob;import React,{useState} from "react";
+import axios from "axios";
+
+function AddJob(){
+
+const [company,setCompany]=useState("");
+const [position,setPosition]=useState("");
+const [status,setStatus]=useState("");
+const [notes,setNotes]=useState("");
+
+const addJob = async ()=>{
+
+await axios.post("http://127.0.0.1:5000/applications",{
+company,
+position,
+status,
+notes
+});
+
+alert("Application Added");
+
+}
+
+return(
+
+<div>
+
+<input placeholder="Company"
+onChange={(e)=>setCompany(e.target.value)}/>
+
+<input placeholder="Position"
+onChange={(e)=>setPosition(e.target.value)}/>
+
+<input placeholder="Status"
+onChange={(e)=>setStatus(e.target.value)}/>
+
+<input placeholder="Notes"
+onChange={(e)=>setNotes(e.target.value)}/>
+
+<button onClick={addJob}>Add</button>
+
+</div>
+
+)
+
+}
+
+export default AddJob;
