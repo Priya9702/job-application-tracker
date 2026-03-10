d94409845b0a70ec796da2319fca25ef145f2476
Priya9702 <110077324+Priya9702@users.noreply.github.com>
2026-03-10 23:01:57 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,14 @@
+import axios from "axios";
 
+const API = axios.create({
+  baseURL: "http://127.0.0.1:5000"
+});
+
+// Get all job applications
+export const getApplications = () => API.get("/applications");
+
+// Add new job application
+export const addApplication = (data) => API.post("/applications", data);
+
+// Delete job application
+export const deleteApplication = (id) => API.delete(`/applications/${id}`);
