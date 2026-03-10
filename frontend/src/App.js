593e90f1062127412c4a83a593e0d026b0d23d32
Priya9702 <110077324+Priya9702@users.noreply.github.com>
2026-03-10 22:56:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,15 @@
+import React from "react";
+import AddJob from "./AddJob";
+import JobList from "./JobList";
 
+function App(){
+return(
+<div>
+<h1>Job Application Tracker</h1>
+<AddJob/>
+<JobList/>
+</div>
+)
+}
+
+export default App;
