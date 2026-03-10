6ae7b70d4bbf1702802ea0cc937276b4bc9e068d
Priya9702 <110077324+Priya9702@users.noreply.github.com>
2026-03-10 22:58:32 +0530

    Update JobList.js

--- frontend/src/JobList.js
+++ frontend/src/JobList.js
@@ -1,1 +1,48 @@
+import React,{useEffect,useState} from "react";
+import axios from "axios";
 
+function JobList(){
+
+const [jobs,setJobs]=useState([]);
+
+const fetchJobs = async ()=>{
+
+const res = await axios.get("http://127.0.0.1:5000/applications");
+
+setJobs(res.data);
+
+}
+
+useEffect(()=>{
+fetchJobs();
+},[])
+
+const deleteJob = async(id)=>{
+
+await axios.delete(`http://127.0.0.1:5000/applications/${id}`);
+
+fetchJobs();
+
+}
+
+return(
+
+<ul>
+
+{jobs.map(job=>(
+<li key={job.id}>
+
+{job.company} - {job.position} - {job.status}
+
+<button onClick={()=>deleteJob(job.id)}>Delete</button>
+
+</li>
+))}
+
+</ul>
+
+)
+
+}
+
+export default JobList;
